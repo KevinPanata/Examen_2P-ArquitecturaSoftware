0dc154acf2cda853f4cb4c3501af813cf9b61f9c
Kevin Panata <kipanata@espe.edu.ec>
2025-12-17 19:48:42 -0500

    Initial commit: Spring Boot project setup with MongoDB and REST API

---
+++ src/main/java/ec/edu/banquito/branchapi/BranchApiApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.banquito.branchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/banquito/branchapi/BranchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.banquito.branchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
